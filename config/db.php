51a8a40ec131699bd3bf9d6e47fb051ecc3afa01
Rafał Majkowski <majkipl@gmail.com>
2026-08-26 17:46:38 +0200

    configure database connection and local environment variables

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'mysql:host=db;dbname=autofleet',
+    'username' => 'autofleet_user',
+    'password' => 'autofleet_password',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
